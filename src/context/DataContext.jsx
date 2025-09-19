4ae41df9f42a51993c2b62f1be92a43053ea68f7
biswabhusan9 <biswabhusanp121@gmail.com>
2025-09-20 00:37:10 +0530

    add the vite config

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import axios from "axios";
 import { createContext, useContext, useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
