6efdc1c6d1d0dfa0082aa22fbb24553d4c1b22b2
biswabhusan9 <biswabhusanp121@gmail.com>
2025-09-20 01:07:44 +0530

    Move Firebase config to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,16 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBL3qOtS7Iz76vx-plgKJZ84vdkg1uEDwI",
-  authDomain: "my-medicine-store-450d6.firebaseapp.com",
-  projectId: "my-medicine-store-450d6",
-  storageBucket: "my-medicine-store-450d6.appspot.com",
-  messagingSenderId: "323257327215",
-  appId: "1:323257327215:web:6117effc1c7a4af1dcb79a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 
